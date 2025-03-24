3b009ba447e9955e0a38b3c2e692544987999024
Christian <seorefchristian@gmail.com>
2025-03-24 22:25:53 +0300

    adding function to manipulate classes

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//Hiding the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Show Modal
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//Looping all the three buttons
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  const element = btnsOpenModal[index];
+
+  //Opening Modal
+  element.addEventListener('click', showModal);
+}
+
+//closing the modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
